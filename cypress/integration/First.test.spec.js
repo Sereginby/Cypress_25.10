bb7324dbabbd544863d1dedecd4f4a0fe30a8ad5
Seregin <seregin_by@mail.ru>
2021-10-26 00:02:48 +0300

    delete all code for master

--- cypress/integration/First.test.spec.js
+++ cypress/integration/First.test.spec.js
@@ -1,46 +1,45 @@
 /// <reference types="Cypress" />
+ describe('Mobile phone replenishment', () => {
 
-// describe('Mobile phone replenishment', () => {
+    it("Show error: Minimum amount of the replenishment 1 UAH", () => {});
+ });
 
-//    it("Show error: Minimum amount of the replenishment 1 UAH", () => {});
-// });
+ }
 
-// }
+ it('By ID', () => {
+    cy.visit("http://www.facebook.com/")
+    cy.get('#email')
+ });    
 
-// it('By ID', () => {
-//    cy.visit("http://www.facebook.com/")
-//    cy.get('#email')
-// });    
+ it('By Class', () => {
+    cy.visit('https://docs.cypress.io/api/table-of-contents')
+    cy.contains('button[type="button"]')
+ });    
 
-// it('By Class', () => {
-//    cy.visit('https://docs.cypress.io/api/table-of-contents')
-//    cy.contains('button[type="button"]')
-// });    
+ it('By Tag', function () {
+   cy.visit('https://docs.cypress.io/api/commands/get#Syntax');
+    cy.get('nav');
+ });    
 
-// it('By Tag', function () {
-//   cy.visit('https://docs.cypress.io/api/commands/get#Syntax');
-//    cy.get('nav');
-// });    
-
-// it('By Tag value', function () {
-//    cy.visit('http://www.facebook.com');
-//    cy.get('[name="pass"]');
-// }); 
+ it('By Tag value', function () {
+    cy.visit('http://www.facebook.com');
+    cy.get('[name="pass"]');
+ }); 
     
-// it('By Diffent Tag', function () {
-//       cy.visit('http://www.facebook.com');
-//        cy.get('[data-testid="open-registration-form-button"][role="button"]');
-//    });        
+it('By Diffent Tag', function () {
+      cy.visit('http://www.facebook.com');
+        cy.get('[data-testid="open-registration-form-button"][role="button"]');
+    });        
 
-// it('By Diffent Types', function () {   
-//    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
-//    cy.get('button[type="button"][title="close banner"]');
-// });    
+ it('By Diffent Types', function () {   
+    cy.visit('https://docs.cypress.io/api/commands/get.html#Syntax')
+   cy.get('button[type="button"][title="close banner"]');
+ });    
 
-// it('By Contains name', () => {
-//    cy.visit('https://next.privat24.ua/')
-//    cy.get('[class^="card"]')
-// });  
+ it('By Contains name', () => {
+    cy.visit('https://next.privat24.ua/')
+    cy.get('[class^="card"]')
+ });
 
 it.only('Using Get with Find and Eq', () => {
     cy.viewport(1000, 660)
